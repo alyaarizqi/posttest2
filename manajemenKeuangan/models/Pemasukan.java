51a5923959a1b96cff37d34e838ad189a78949e6
Alya Rizqi Ramadhani <144756486+alyaarizqi@users.noreply.github.com>
2024-10-14 09:14:31 +0800

    Update Pemasukan.java

--- manajemenKeuangan/models/Pemasukan.java
+++ manajemenKeuangan/models/Pemasukan.java
@@ -1,7 +1,18 @@
 package manajemenKeuangan.models;
 
 public class Pemasukan extends Transaksi {
-    public Pemasukan(double jumlah, String keterangan) {
-        super(jumlah, "Pemasukan", keterangan);
+    private final String sumberPemasukan;
+
+    public Pemasukan(double jumlah, String keterangan, String sumberPemasukan) {
+        super(jumlah, keterangan);
+        this.sumberPemasukan = sumberPemasukan;
+        this.jenis = "Pemasukan";
+    }
+
+    @Override
+     public void tampilkanTransaksi() {
+        System.out.println("Pemasukan: Rp" + jumlah); 
+        System.out.println("Keterangan: " + keterangan); 
+        System.out.println("Sumber: " + sumberPemasukan);
     }
 }
