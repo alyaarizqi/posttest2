285beba484e4a53007187dbfe4faa28000546bfa
Alya Rizqi Ramadhani <144756486+alyaarizqi@users.noreply.github.com>
2024-10-14 09:15:01 +0800

    Update Pengeluaran.java

--- manajemenKeuangan/models/Pengeluaran.java
+++ manajemenKeuangan/models/Pengeluaran.java
@@ -1,7 +1,18 @@
 package manajemenKeuangan.models;
 
 public class Pengeluaran extends Transaksi {
-    public Pengeluaran(double jumlah, String keterangan) {
-        super(jumlah, "Pengeluaran", keterangan);
+    private final String metodePembayaran;
+
+    public Pengeluaran(double jumlah, String keterangan, String metodePembayaran) {
+        super(jumlah, keterangan);
+        this.metodePembayaran = metodePembayaran;
+        this.jenis = "Pengeluaran";
+    }
+
+    @Override
+    public void tampilkanTransaksi() {
+        System.out.println("Pengeluaran: Rp" + jumlah); 
+        System.out.println("Keterangan: " + keterangan); 
+        System.out.println("Metode: " + metodePembayaran);
     }
 }
